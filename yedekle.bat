69ad50c44ea144f552a98f885292b86a07260acd
tifil123 <berkanuzun29@gmail.com>
2025-12-09 21:48:02 +0300

    İlk yedekleme - chatbot projesi dosyaları ve otomatik yedekleme script'i eklendi

---
+++ yedekle.bat
@@ -0,0 +1,31 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Değişkenleri ayarla
+set PROJE_KLASORU=C:\Users\berka\Downloads\chatbot
+set GITHUB_KULLANICI=kullanici_adi
+set DEPO_ADI=chatbot-yedekleme
+set TARIH=%date:~-4%-%date:~4,2%-%date:~7,2%
+
+:: Log dosyası oluştur
+echo [%time%] Yedekleme baslatildi >> %PROJE_KLASORU%\yedekleme.log
+
+:: GitHub'da depo yoksa oluştur
+cd /d %PROJE_KLASORU%
+if not exist ".git" (
+    echo [%time%] Git deposu初始化 ediliyor >> %PROJE_KLASORU%\yedekleme.log
+    git init
+    git remote add origin https://github.com/%GITHUB_KULLANICI%/%DEPO_ADI%.git
+)
+
+:: Tüm dosyaları ekle ve commit yap
+echo [%time%] Dosyalar yukleniyor >> %PROJE_KLASORU%\yedekleme.log
+git add .
+git commit -m "Otomatik yedekleme - %TARIH%"
+
+:: GitHub'a gönder
+echo [%time%] GitHub'a yukleniyor >> %PROJE_KLASORU%\yedekleme.log
+git push origin main
+
+echo [%time%] Yedekleme tamamlandi >> %PROJE_KLASORU%\yedekleme.log
+echo Yedekleme basariyla tamamlandi!
